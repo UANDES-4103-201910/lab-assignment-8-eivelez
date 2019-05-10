2a43b1b9ef148646be38b72146321eb23f7ac29d
eivelez <eivelez@miuandes.cl>
2019-05-09 22:10:10 -0400

    Upper and lower case function added!

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,14 +1,25 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+	var shift=0;
 	function KeyboardController(){
 	  $(".key").click(function()
 	  {
-	    var x =$(".console");
-	    var y =document.getElementsByClassName("output");
-	    y[0].innerHTML+=$(this).text();
-	    var clickedBtnclass = $(this).attr("class");
+	    //var x =$(".console"); (other way)
+	    //var clickedBtnclass = $(this).attr("class"); (getting attr)
+	    var pressedKey = $(this);
+	    var pressedKeyText = pressedKey.text();
+	    var console =document.getElementsByClassName("output");
 
+	    if(pressedKey.attr("class")=="key bksp"){
+	    } else if(pressedKey.attr("class")=="key shift special"){
+		shift=1;
+	    } else if(shift==1){
+		console[0].innerHTML+=pressedKeyText;
+		shift=0;
+	    } else {
+		console[0].innerHTML+=pressedKeyText.toLowerCase();
+	    } 
 	  });
 	}
 	KeyboardController();
