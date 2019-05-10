06a2f791c875542034044a2b162a88dbe9109a2a
eivelez <eivelez@miuandes.cl>
2019-05-09 20:39:03 -0400

    Update keyboard.js

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,15 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+	function KeyboardController()
+	{
+	  $(".key").click(function()
+	  {
+	    var clickedBtnclass = $(this).attr("class")//save 4 use;
+
+	    alert($(this).text());
+	  });
+	};
+	
+	KeyboardController();
 });
