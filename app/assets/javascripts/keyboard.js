53159af43da812e8893bb0980af0a6fd859946ba
eivelez <eivelez@miuandes.cl>
2019-05-09 23:26:40 -0400

    Update keyboard.js

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -2,6 +2,7 @@
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
 	var shift=0;
+	var hiddenK=0;
 	function KeyboardController(){
 	  $(".key").click(function()
 	  {
@@ -12,15 +13,38 @@ $(() => {
 	    var console =document.getElementsByClassName("output");
 
 	    if(pressedKey.attr("class")=="key bksp"){
-	    } else if(pressedKey.attr("class")=="key shift special"){
+	    }else if(pressedKey.attr("class")=="key shift special"){
 		shift=1;
-	    } else if(shift==1){
+		pressedKey.css('background-color', 'RGB(255,0,0)');
+		shiftKey = pressedKey;
+	    }else if(shift==1){
 		console[0].innerHTML+=pressedKeyText;
 		shift=0;
-	    } else {
+		shiftKey.css('background-color', 'RGB(64,255,0)');
+	    }else{
 		console[0].innerHTML+=pressedKeyText.toLowerCase();
-	    } 
+	    }
 	  });
 	}
 	KeyboardController();
+	function KeyboardHide(){
+	  $(".toogle").click(function()
+	  {
+	    var keyboard =$(".kcontainer");
+	    var keys =$(".keys");
+	    if (hiddenK==0){
+
+	      keyboard.css('visibility','hidden');
+	      hiddenK=1;
+	      $(this).css("margin-top","80vh");
+
+	    }else if (hiddenK==1){
+	      keyboard.css('visibility','visible');
+	      hiddenK=0;
+	      $(this).css("margin-top","55vh");
+	    }
+
+	  });
+	}
+	KeyboardHide();
 });
